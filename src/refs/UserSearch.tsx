3310fda26cf2be2fe29c56e9b9ea3f719ce97b9a
Dawid Lipiński <dlipinski@arex.local>
2021-02-01 14:08:54 +0100

    First stage

---
+++ src/refs/UserSearch.tsx
@@ -0,0 +1,42 @@
+import { useState, useRef, useEffect } from 'react';
+
+const users = [
+	{ name: 'Sarah', age: 20 },
+	{ name: 'Alax', age: 20 },
+	{ name: 'Micheal', age: 20 },
+];
+
+const UserSearch: React.FC = () => {
+	const inputRef = useRef<HTMLInputElement | null>(null);
+	const [name, setName] = useState('');
+	const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+	const onClick = () => {
+		const foundUser = users.find(user => {
+			return user.name === name;
+		});
+
+		setUser(foundUser);
+  };
+  
+  useEffect(() => {
+    if (!inputRef.current) {
+      return;
+    }
+    inputRef.current.focus();
+  }, []);
+
+	return (
+		<div>
+			User Search
+			<input value={name} onChange={e => setName(e.target.value)} ref={inputRef} />
+			<button onClick={onClick}>Find User</button>
+			<div>
+				{user && user.name}
+				{user && user.age}
+			</div>
+		</div>
+	);
+};
+
+export default UserSearch;
